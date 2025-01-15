58ac2052406b90c2a2a975a96f04312cb26a2fe3
Md Anisul Islam <mdanisul55@gmail.com>
2025-01-15 11:19:33 +0600

    sixth

--- django/third_project/first_app/views.py
+++ django/third_project/first_app/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def details(request):
-    return render (request,'./first_app/index.html', context=
-                   {'Student':'Anis', 'Roll':'112063', 'Sec':'B', 'Age':10},
+    return render (request,'./first_app/index.html', {'Courses':[
+        {'course':'Python', 'price':100},
+        {'course':'Java', 'price':200},
+        {'course':'C++', 'price':300},
+        {'course':'C#', 'price':400},
+        
+    ]},
+                   
+                   #this is for if else condition. This line are transfering data
+                   #from views to html template or backend to frontend
+                # context={'Student':'Anis', 'Roll':'112063', 'Sec':'B', 'Age':10},
                    )
