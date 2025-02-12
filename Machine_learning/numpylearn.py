1efe48497b349d32671b62796aa75580f72e451f
Md Anisul Islam <mdanisul55@gmail.com>
2025-02-12 17:33:09 +0600

    change

---
+++ Machine_learning/numpylearn.py
@@ -0,0 +1,21 @@
+"""
+0 D array
+
+import numpy as np
+# arr= numpy.array(1)
+# print(arr)  # Output: [1]
+# print(arr.ndim) #nth dimension Output: [0]
+"""
+#1D array in numpy
+import numpy as np
+# arr= numpy.array([1,2,3,4])
+# print(arr)  # Output: [1 2 3 4]
+# print(arr.ndim) #nth dimension 
+
+"""
+This are called 2D matrices.
+They are used to represent data in a table format.
+"""
+arraye= np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arraye)
+print(arraye.ndim)
